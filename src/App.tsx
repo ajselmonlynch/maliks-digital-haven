31475279b6a5a4fdd9cda715bc97f71ccfadd0f3
ajselmonlynch <240105894+ajselmonlynch@users.noreply.github.com>
2026-04-05 22:38:08 -0400

    Configure base path for GitHub Pages deployment

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/maliks-digital-haven">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/journal" element={<Journal />} />
